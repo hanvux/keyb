9146b66a5ac9b088b76ba3a8f461657ddcc61aac
hanvux <120507485+hanvux@users.noreply.github.com>
2022-12-25 22:27:54 +0700

    Update routes.py

--- key/routes.py
+++ key/routes.py
@@ -1,59 +1,98 @@
-
-from flask import Blueprint, render_template, request, redirect, Flask
-app = Flask(__name__, template_folder='templates',static_folder='static')  
 import random
-import os
 import sys
+from tkinter import *
+from tkinter.ttk import *
+#https://www.geeksforgeeks.org/open-a-new-window-with-a-button-in-python-tkinter/?ref=gcse connect two pages
+import PySimpleGUI as sg
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
 
-@app.route("/analyze", methods=['POST'])
+win = Tk()
 
-def analyze():
-    def markov_lib(key, character1):
-        data_sample = "moby-dick.txt"
-        text_data = open(data_sample, 'r').read()
-        text_data = ''.join([i for i in text_data if not i.isdigit()]).replace("\n", " ").split(' ')
-        markov_lib = {}
 
-        for i in range(len(text_data)-key):
-            word = " ".join(text_data[i:i+key])
-            if word.lower() in markov_lib.keys():
-                markov_lib[word.lower()].append(text_data[i+key])
-            else:
-                markov_lib[word.lower()] = [text_data[i+key]]
 
-        try:
-            character2 = random.choice(markov_lib[character1.lower()])
-        except KeyError as e:
-            return ("fail")
-        return character2
-    key = int(request.form['accuracy'])
-    first_word = str(request.form['text'])
+# type this if want the button shown on output field
+#prediction.set(expression)
+
+def keyboard():
+    layout = [
+    [sg.Button('Tab')],
+    [sg.Button('Nobe')],
+    [sg.Button('End')],
+    ]
+
+    window = sg.Window('Keyboard', layout, finalize=True, return_keyboard_events=True)
+
+
+    key = int(radio.get())
+    first_word = str(wordArea.get())
+
+
     message = first_word
+
+    data_sample = "215.txt"
+    text_data = open(data_sample, 'r').read()
+    text_data = ''.join([i for i in text_data if not i.isdigit()]).replace("\n", " ").split(' ')
+    markov_lib = {}
+
+    for i in range(len(text_data) - key):
+        word = " ".join(text_data[i:i + key])
+        if word.lower() in markov_lib.keys():
+            markov_lib[word.lower()].append(text_data[i + key])
+        else:
+            markov_lib[word.lower()] = [text_data[i + key]]
+
+
     while(True):
-        os.system('cls')
         first_word = " ".join(message.split()[0-key:])
-        predicted_next_word = markov_lib(key,first_word)
-
-        if predicted_next_word == "fail":
-            print("-------------------------\nThe training text is not big enough to predict the next word. Exited")
-        response = str(request.form['res'])
+        try:
+            predicted_next_word = random.choice(markov_lib[first_word.lower()])
+        except KeyError as e:
+            result.insert(1.0, "-------------------------\nThe training text is not big enough to predict the next word. Exited")
+            sys.exit(1)
         
-        if response == "t" or response == "T":
-            #os.system('cls')
-            message = message + " " + predicted_next_word
-        if response == "f" or response == "F":
-            os.system('cls')
-            r = message + " " + {request.form['res']}
-            message = message + " " + r
+        r = message +" ["+predicted_next_word+"] "
+        result.insert(1.0, r)
+        
+        event, values = window.read()
 
-        if response == "E" or response == "e":
-            print(message)
+
+        if event == "Tab":
+            message = message + " " + predicted_next_word
+            result.insert(1.0, message)
+        if event == "Nobe":
+            response = message + " " + {resArea.get()}
+            message = message + " " +response
+            result.insert(1.0, message)
+
+        if event == "End":
+            result.insert(1.0, message)
             break
-    render_template('index.html', message = message)
-if __name__=='__main__':
-   app.run(debug=True)
+
+
+
+win.geometry('600x600')
+prediction = StringVar()
+
+radio = IntVar()
+Label(text="Accuracy level", font=('Aerial 11')).pack()
+r1 = Radiobutton(win, text="Accuracy level 1", variable=radio, value=1)
+r1.pack(anchor=N)
+r2 = Radiobutton(win, text="Accuracy level 2", variable=radio, value=2)
+r2.pack(anchor=N)
+r3 = Radiobutton(win, text="Accuracy level 3", variable=radio, value=3)
+r3.pack(anchor=N)
+
+
+wlbl = Label(win, text = "Input word")
+wlbl.pack()
+wordArea = Entry(win, width=10)
+wordArea.pack()
+
+
+lbl = Label(win, text = "Result")
+lbl.pack()
+result = Text(win)
+result.place(y=370, relx=.5, anchor="c", width=450, height=70)
+button_s = Button(win, text=' Submit ', command=keyboard).pack()
+win.mainloop()
